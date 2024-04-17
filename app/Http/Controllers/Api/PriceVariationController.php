a48074a815c4d38a3be9c25f002033c73e81315f
Geronimo <gerobasso01@gmail.com>
2024-04-17 11:06:44 +0200

    Added the getPlayerValue table

--- app/Http/Controllers/Api/PriceVariationController.php
+++ app/Http/Controllers/Api/PriceVariationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Player;
 use App\Models\PriceVariation;
 use Illuminate\Http\Request;
 
@@ -10,10 +11,14 @@ class PriceVariationController extends Controller
 {
     public function getPlayerValue($player_id)
     {
-        $price_variations = PriceVariation::where('id_mundo_deportivo', $player_id)->get();
+        $player = Player::where('id_mundo_deportivo', $player_id)->first(['id_mundo_deportivo', 'full_name', 'player_value', 'photo_body', 'photo_face']);
+
+        $price_variations = PriceVariation::where('id_mundo_deportivo', $player_id)->get(['price_day','price','is_prediction']);
         if ($price_variations->isEmpty()) {
             return response()->json(['message' => 'No price information found for this player'], 404);
         }
-        return response()->json($price_variations);
+
+        $player->price_variations = $price_variations;
+        return response()->json($player);
     }
 }
